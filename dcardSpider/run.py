3576d1e750bcbdfaf84b0e4df6382a11ff827a7d
CharlieDa <charlieda@tedu.tw>
2020-05-26 21:15:40 +0800

    remove count.txt if count.txt exist

--- dcardSpider/run.py
+++ dcardSpider/run.py
@@ -1,2 +1,7 @@
 from scrapy import cmdline
+import os
+import logging
+if os.path.isfile('count.txt'):
+	os.remove('count.txt')
+	logging.info('count.txt is removed.')
 cmdline.execute('scrapy crawl DcardSpider'.split())
